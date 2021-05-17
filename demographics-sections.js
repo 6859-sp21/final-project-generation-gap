13eaf1b81d9341620f158f8be2d4dfd2e7166771
Jessica Lee <jlee22@mit.edu>
2021-05-17 03:26:09 -0400

    added cleaned datasets, reformatted timeline

--- demographics-sections.js
+++ demographics-sections.js
@@ -1,110 +1,108 @@
-
 var main = d3.select("main");
 var demographicsScrolly = main.select("#demographics-scrolly");
 var demographicsFigure = demographicsScrolly.select("#demographics-figure");
-var demographicsGenZFigure = demographicsFigure.select("#demographics-gen-z-figure");
-var demographicsBoomerFigure = demographicsFigure.select("#demographics-boomer-figure");
+var demographicsGenZFigure = demographicsFigure.select(
+  "#demographics-gen-z-figure"
+);
+var demographicsBoomerFigure = demographicsFigure.select(
+  "#demographics-boomer-figure"
+);
 var demographicsArticle = demographicsScrolly.select("#demographics-article");
 var demographicsStep = demographicsArticle.selectAll(".step");
 
-
 var scroller = scrollama();
 
-var genzTitle = d3.select('#gen-z-title')
-var boomerTitle = d3.select('#boomer-title')
-var genzText = d3.select('#gen-z-text')
-var boomerText = d3.select('#boomer-text')
-var genzSVG = d3.select('#gen-z-area').append('svg').attr('width', 400).attr('height', 400)
-var boomerSVG = d3.select('#boomer-area').append('svg').attr('width', 400).attr('height', 340)
-
+var genzTitle = d3.select("#gen-z-title");
+var boomerTitle = d3.select("#boomer-title");
+var genzText = d3.select("#gen-z-text");
+var boomerText = d3.select("#boomer-text");
 
 function handleResize() {
-    // 1. update height of step elements
-    var stepH = Math.floor(window.innerHeight * 0.4);
-    demographicsStep.style("height", stepH + "px");
-  
-    var demographicsFigureHeight = window.innerHeight * .8;
-    var demographicsFigureMarginTop = (window.innerHeight - demographicsFigureHeight) / 2;
+  // 1. update height of step elements
+  var stepH = Math.floor(window.innerHeight * 0.4);
+  demographicsStep.style("height", stepH + "px");
 
+  var demographicsFigureHeight = window.innerHeight * 0.8;
+  var demographicsFigureMarginTop =
+    (window.innerHeight - demographicsFigureHeight) / 2;
+
+  demographicsGenZFigure
+    .style("height", demographicsFigureHeight + "px")
+    .style("width", 50 + "%");
+  demographicsBoomerFigure
+    .style("height", demographicsFigureHeight + "px")
+    .style("width", 50 + "%");
+
+  demographicsFigure
+    .style("height", demographicsFigureHeight + "px")
+    .style("top", demographicsFigureMarginTop + "px");
+
+  // 3. tell scrollama to update new element dimensions
+  scroller.resize();
+}
+
+function handleDemographicsStepEnter(response) {
+  if (response.index == 0) {
     demographicsGenZFigure
-        .style("height", demographicsFigureHeight + "px")
-        .style("width", 50 + "%");
+    .selectAll("div")
+    .style("opacity", 0.0)
     demographicsBoomerFigure
-        .style("height", demographicsFigureHeight + "px")
-        .style("width", 50 + "%");
-  
-    demographicsFigure
-      .style("height", demographicsFigureHeight + "px")
-      .style("top", demographicsFigureMarginTop + "px");
-  
-    // 3. tell scrollama to update new element dimensions
-    scroller.resize();
+    .selectAll("div")
+    .style("opacity", 0.0)
+  } else if (response.index == 1) {
+    demographicsGenZFigure
+    .selectAll("div")
+    .style("opacity", 0.0)
+    demographicsBoomerFigure
+    .selectAll("div")
+    .style("opacity", 0.0)
+    genzTitle.text("Generation Z");
+    boomerTitle.text("Baby Boomers");
+  } else if (response.index == 2) {
+    genzTitle.text("Generation Z");
+    boomerTitle.text("Baby Boomers");
+    demographicsGenZFigure
+    .selectAll("div")
+    .style("opacity", 0.0)
+    demographicsBoomerFigure
+    .selectAll("div")
+    .style("opacity", 0.0)
+  } else if (response.index == 3) {
+    demographicsGenZFigure
+    .selectAll("div")
+    .style("opacity", 1.0)
+    demographicsBoomerFigure
+    .selectAll("div")
+    .style("opacity", 1.0)
+    // Iraq War
+  } else if (response.index == 4) {
+    // TIMELINE SIMILARITY 1
+    // First iPhone released
+    demographicsGenZFigure
+    .select("#gen-z-1")
+    .attr("color", "red")
+  } else if (response.index == 5) {
+    // TIMELINE SIMILARITY 2
+    // Sandy Hook Shooting
+
+
+  } else if (response.index == 6) {
+    // Black Lives Matter Protests during Ferguson Unrest
+  } else if (response.index == 7) {
+    // SpaceX Launches
   }
+}
 
-  function handleDemographicsStepEnter(response) {
-    demographicsStep.classed("is-active", function(d, i) {
-        return i === response.index;
-      });
+function init() {
+  handleResize();
 
-    console.log(response.index);
-    if (response.index == 0) {
-      genzSVG.selectAll('image').remove()
-      boomerSVG.selectAll('image').remove()
-      genzTitle.text('')
-      boomerTitle.text('')
-    } else if (response.index == 1) {
-      // genzTitle.text('GenZ Age Range')
-      // genzText.text('')
-      // genzSVG.selectAll('image').remove()
-      // genzSVG.append('image')
-      // .attr('xlink:href', 'genz_age.png')
-      // .attr('width', 370).attr('height', 300)
-      // boomerTitle.text('Boomer Age Range')
-      // boomerText.text('')
-      // boomerSVG.selectAll('image').remove()
-      // boomerSVG.append('image')
-      // .attr('xlink:href', 'boomer_age.png')
-      // .attr('width', 370).attr('height', 300)
+  scroller
+    .setup({
+      step: "#demographics-scrolly #demographics-article .step",
+      offset: 0.5,
+    })
+    .onStepEnter(handleDemographicsStepEnter);
 
-
-    } else if (response.index == 2) {
-      // genzTitle.text('GenZ Population: 68 Million')
-      // .attr('xlink:href', 'genz_population.png')
-      // .attr('width', 400).attr('height', 330)
-      // boomerTitle.text('Boomer Population: 71.6 Million')
-      // boomerText.text('')
-      // boomerSVG.selectAll('image').remove()
-      // boomerSVG.append('image')
-      // .attr('xlink:href', 'boomer_population.png')
-      // .attr('width', 400).attr('height', 330)
-
-    } else if (response.index == 3) {
-      // TIMELINE DIFFERENCES
-      
-
-
-    } else if (response.index == 4) {
-      // TIMELINE SIMILARITY 1
-      // TECHNOLOGY
-    } else if (response.index == 5) {
-      // TIMELINE SIMILARITY 2
-      // BLM + CIVIL RIGHTS MOVEMENT
-    } else if (response.index == 6) {
-      
-    }
-  }
-  
-  function init() {
-    handleResize();
-  
-    scroller
-      .setup({
-        step: "#demographics-scrolly #demographics-article .step",
-        offset: 0.5,
-      })
-      .onStepEnter(handleDemographicsStepEnter);
-  
-  
-    window.addEventListener("resize", handleResize);
-  }
-  init();
+  window.addEventListener("resize", handleResize);
+}
+init();
