118517936fe8e9961d4698266d5b5d49704a99b1
Annie Zhang <annie1blue@gmail.com>
2021-05-17 03:34:04 -0400

    changed modal to new design

--- final-vis.js
+++ final-vis.js
@@ -7,15 +7,14 @@ const final_margin = { top: 50, right: 50, bottom: 50, left: 100 },
   newsHeight = 100,
   maxLineNumber = 7,
   headerContainerWidth = 60,
-  headerContainerMargin = {top: 1, left: 1}
-  strokeWidth = 0,
-  numRow = 10,
-  numFilters = 10,
+  headerContainerMargin = { top: 1, left: 1 };
+(strokeWidth = 0),
+  (numRow = 10),
+  (numFilters = 10),
   //Square and Highlight Colors
-  squareColor = "rgba(198, 198, 198, .5)",
-  curSquareColor = squareColor,
-  highlightColor = "grey";
-
+  (squareColor = "rgba(198, 198, 198, .5)"),
+  (curSquareColor = squareColor),
+  (highlightColor = "grey");
 
 // var boomerData;
 // var genZData;
@@ -33,7 +32,6 @@ const final_margin = { top: 50, right: 50, bottom: 50, left: 100 },
 // setTimeout(function(){
 //   console.log(genZData);
 // },200);
-
 
 // function getSources(age, region, metro, sex, education, race) {
 //   // see what sourceuse
@@ -64,25 +62,25 @@ const final_margin = { top: 50, right: 50, bottom: 50, left: 100 },
 
 // }
 
-  var biasColors = {
-    "Left": "#2E65A0",
-    "Lean Left": "#9EC8EB",
-    "Center": "#9766A0",
-    "Lean Right": "#CA9A98",
-    "Right": "#CB2127",
-  };
+var biasColors = {
+  Left: "#2E65A0",
+  "Lean Left": "#9EC8EB",
+  Center: "#9766A0",
+  "Lean Right": "#CA9A98",
+  Right: "#CB2127",
+};
 
 var row = d3.scaleLinear().domain([0, numRow]).range([0, 1100]);
 
 var filters = {
-    bias: [],
-    topic: [],
-  };
+  bias: [],
+  topic: [],
+};
 
-  var filtersDefault = {
-    bias: ["Left", "Lean Left", "Center", "Lean Right", "Right"],
-    topic: ["covid", "climate+change", "blm", "guns", "economy"],
-  };
+var filtersDefault = {
+  bias: ["Left", "Lean Left", "Center", "Lean Right", "Right"],
+  topic: ["covid", "climate+change", "blm", "guns", "economy"],
+};
 
 var svg = d3
   .select("#final_news")
@@ -93,79 +91,77 @@ var svg = d3
 //   .attr("transform", "translate(" + final_margin.left + "," + final_margin.top + ")");
 
 var tooltip = d3
-.select("body")
-.append("div")
-.attr("class", "tooltip")
-.style("opacity", 0);
+  .select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
 
 function updateFilter() {
-    biasFilter = ["Left", "Lean Left", "Center", "Lean Right", "Right"];
-    topicFilter = ["covid", "climate+change", "blm", "guns", "economy"];
+  biasFilter = ["Left", "Lean Left", "Center", "Lean Right", "Right"];
+  topicFilter = ["covid", "climate+change", "blm", "guns", "economy"];
 
-    console.log(document.getElementById("Left").checked)
-  
-    filters["bias"] = biasFilter.map((f) => {
-      if (document.getElementById(f).checked == true) return f;
-    });
-    filters["topic"] = topicFilter.map((f) => {
-      if (document.getElementById(f).checked == true) return f;
-    });
+  console.log(document.getElementById("Left").checked);
 
-    noSelectCount = 0;
-    filterTitle = "";
-    for (filterKey in filters) {
-      var filterCount = 0;
-      var curFilter = filters[filterKey];
-      for (f in curFilter) {
-        if (curFilter[f] === undefined) filterCount += 1;
-        // else {
-        //   filterTitle += titleMap[curFilter[f].toString()] + ", ";
-        // }
-      }
-      noSelectCount += filterCount;
-      if (filterCount === curFilter.length) {
-        filters[filterKey] = filtersDefault[filterKey];
-      }
+  filters["bias"] = biasFilter.map((f) => {
+    if (document.getElementById(f).checked == true) return f;
+  });
+  filters["topic"] = topicFilter.map((f) => {
+    if (document.getElementById(f).checked == true) return f;
+  });
+
+  noSelectCount = 0;
+  filterTitle = "";
+  for (filterKey in filters) {
+    var filterCount = 0;
+    var curFilter = filters[filterKey];
+    for (f in curFilter) {
+      if (curFilter[f] === undefined) filterCount += 1;
+      // else {
+      //   filterTitle += titleMap[curFilter[f].toString()] + ", ";
+      // }
     }
-
-    if (noSelectCount === numFilters) {
-        filters["bias"] = [];
-        filters["topic"] = [];
-      }
-
-    console.log('filters', filters)
-  
-    return filters;
+    noSelectCount += filterCount;
+    if (filterCount === curFilter.length) {
+      filters[filterKey] = filtersDefault[filterKey];
+    }
   }
 
-  function highlighted(data) {
-    filtered_data = data.filter((d) => fitsFilter(d));
-    rest_of_data = data.filter((d) => !filtered_data.includes(d));
-  
-    const all_data = {};
-    var i = 0;
-    for (key in filtered_data) {
-      if (filtered_data.hasOwnProperty(key)) {
-        // all_data[i] = filtered_data[key];
-        all_data[filtered_data[key].Headline] = biasColors[filtered_data[key].Bias]
-        i++;
-      }
-    }
-    for (key in rest_of_data) {
-      if (rest_of_data.hasOwnProperty(key)) {
-        // all_data[i] = rest_of_data[key];
-        all_data[rest_of_data[key].Headline] = squareColor
-        // i++;
-      }
-    }
-    return all_data;
+  if (noSelectCount === numFilters) {
+    filters["bias"] = [];
+    filters["topic"] = [];
   }
-  function fitsFilter(d) {
-    return (
-      filters["bias"].includes(d.Bias) &&
-      filters["topic"].includes(d.Topic)
-    );
+
+  console.log("filters", filters);
+
+  return filters;
+}
+
+function highlighted(data) {
+  filtered_data = data.filter((d) => fitsFilter(d));
+  rest_of_data = data.filter((d) => !filtered_data.includes(d));
+
+  const all_data = {};
+  var i = 0;
+  for (key in filtered_data) {
+    if (filtered_data.hasOwnProperty(key)) {
+      // all_data[i] = filtered_data[key];
+      all_data[filtered_data[key].Headline] =
+        biasColors[filtered_data[key].Bias];
+      i++;
+    }
   }
+  for (key in rest_of_data) {
+    if (rest_of_data.hasOwnProperty(key)) {
+      // all_data[i] = rest_of_data[key];
+      all_data[rest_of_data[key].Headline] = squareColor;
+      // i++;
+    }
+  }
+  return all_data;
+}
+function fitsFilter(d) {
+  return filters["bias"].includes(d.Bias) && filters["topic"].includes(d.Topic);
+}
 
 // function render() {
 //   d3.csv("data/allsides.csv").then(function (data) {
@@ -208,19 +204,19 @@ window.addEventListener("click", function (e) {
 
 // RENDER
 function render() {
-//   getSources(
-//     "65+",
-//     "Midwest",
-//     "Metropolitan",
-//     "Female",
-//     "College graduate+",
-//     "Asian or Asian-American"
-//   );
+  //   getSources(
+  //     "65+",
+  //     "Midwest",
+  //     "Metropolitan",
+  //     "Female",
+  //     "College graduate+",
+  //     "Asian or Asian-American"
+  //   );
 
   d3.csv("./data/allsides.csv").then(function (data) {
     svg.selectAll("g").remove();
-    highlightedData = highlighted(data)
-    updateFilter()
+    highlightedData = highlighted(data);
+    updateFilter();
 
     var g = svg
       .selectAll("g")
@@ -258,7 +254,7 @@ function render() {
     //   // .on("mouseout", handleMouseOut_died)
     //   .on("click", handleClick);
 
-      g.append("image")
+    g.append("image")
       .attr("xlink:href", "img/newspaper_icon.png")
       .attr("x", (d, i) => {
         const n = i % numRow;
@@ -274,7 +270,7 @@ function render() {
       .attr("height", newsHeight)
       .attr("stroke-width", strokeWidth)
       .attr("stroke", squareColor)
-      .attr("fill", squareColor)
+      .attr("fill", squareColor);
 
     g.append("text")
       .attr("x", (d, i) => {
@@ -295,8 +291,8 @@ function render() {
       .attr("font-size", "9px")
       .call(wrap, headerContainerWidth);
 
-      g.append("rect")
-    //   .attr("xlink:href", "img/newspaper_icon.png")
+    g.append("rect")
+      //   .attr("xlink:href", "img/newspaper_icon.png")
       .attr("x", (d, i) => {
         const n = i % numRow;
         return row(n);
@@ -311,8 +307,10 @@ function render() {
       .attr("height", newsHeight)
       .attr("stroke-width", strokeWidth)
       .attr("stroke", squareColor)
-    //   .attr("fill", (d) => {return biasColors[d.Bias]})
-      .attr("fill", (d) => {return highlightedData[d.Headline]})
+      //   .attr("fill", (d) => {return biasColors[d.Bias]})
+      .attr("fill", (d) => {
+        return highlightedData[d.Headline];
+      })
       .attr("opacity", 0.3)
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut)
@@ -353,15 +351,45 @@ function render() {
   function handleClick(d) {
     $("#myModal").modal({
       fadeDuration: 300,
-    }).html(`<h1 id="modal_name">${d.Headline}</h1>
-                      <h1 id="modal_info" style="color:${
-                        biasColors[d.Bias]
-                      }">${d.Bias}</h1>
-                      <h1 id="modal_info">Source: ${d.Source}</h1>
-                      <h1 id="modal_info">Date: ${d.Date}</h1>
-                      <div style="text-align:center"><a href=${
-                        d.URL
-                      } class="button1" target="blank">Read this story</a></div>`);
+    }).html(`<h1 id="source_name">${d.Source}</h1><h1 id="modal_name">${
+      d.Headline
+    }</h1>
+    <h1 id="modal_byline">${d.Byline}</h1>                  
+    <h1 id="modal_info">Date: ${d.Date}</h1>
+    <h1 id="modal_info">Topic: ${d.Topic}</h1>
+    <h1 id="modal_info" style="color:${biasColors[d.Bias]}">Media Bias: ${
+      d.Bias
+    }</h1>
+    <div style="text-align:center"><a href=${
+      d.URL
+    } class="button1" target="blank" style="background-color:${
+      biasColors[d.Bias]
+    }">Read this story</a></div>
+    <h1 id="modal_name">Recommended Reads:</h1>  
+    <div style="align-items: center; flex-direction: column; display: flex;">
+    <div class="line-container">
+    <span class="line arrow-left"></span>
+    <label>Media Bias</label>
+    <span class="line arrow-right"></span>
+  </div>
+    <div style="justify-items: space-between; flex-direction: row; display: flex;">
+
+    <div style="text-align:center;"><a style="background-color:${
+      biasColors["Left"]
+    }" href=${d.URL} class="button1" target="blank">Read this story</a></div> 
+
+    <div style="text-align:center;"><a style="background-color:${
+      biasColors["Center"]
+    }" href=${d.URL} class="button1" target="blank">Read this story</a></div> 
+
+    <div style="text-align:center;"><a style="background-color:${
+      biasColors["Right"]
+    }" href=${d.URL} class="button1" target="blank">Read this story</a></div> 
+    
+    </div>
+    
+    </div>
+    `);
   }
 }
 
@@ -391,27 +419,31 @@ function wrap(text, width) {
         line.pop();
         tspan.text(line.join("\n"));
         line = [word];
-        if (lineNumber<maxLineNumber) { 
-            tspan = text
+        if (lineNumber < maxLineNumber) {
+          tspan = text
             .append("tspan")
             .attr("x", x)
             .attr("y", y)
             .attr("dx", headerContainerMargin.left + "em")
-            .attr("dy", ++lineNumber * lineHeight + headerContainerMargin.top + dy + "em")
+            .attr(
+              "dy",
+              ++lineNumber * lineHeight + headerContainerMargin.top + dy + "em"
+            )
             .text(word);
-        }
-        else {
-            tspan = text
+        } else {
+          tspan = text
             .append("tspan")
             .attr("x", x)
             .attr("y", y)
             .attr("dx", headerContainerMargin.left + "em")
-            .attr("dy", ++lineNumber * lineHeight + headerContainerMargin.top + dy + "em")
+            .attr(
+              "dy",
+              ++lineNumber * lineHeight + headerContainerMargin.top + dy + "em"
+            )
             .text("...");
-            break
+          break;
         }
-        
-      }   
+      }
     }
   });
 }
